9c73768ee7a52d6d96c18ad9cd9754f3bfaf22ad
RushikeshTekhnologia <rushikesh.patil@gmail.com>
2026-01-14 12:45:17 +0530

    parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import ParallaxComp from "./components/parallax/ParallaxComp"
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <p className='bg-amber-300'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores reprehenderit laboriosam quas quis harum blanditiis? Libero esse debitis, laboriosam impedit minus perferendis dolorum adipisci accusantium voluptate odio facere, recusandae explicabo.</p>
-      </div>
+      <ParallaxComp />
     </>
   )
 }
---
+++ src/components/TextBlock.tsx
@@ -0,0 +1,15 @@
+import "../App.css"
+
+function TextBlock() {
+  return (
+    <div id="textblock">
+      <div id="textblock-container">
+        <p id="textblock-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, pariatur aperiam. Deserunt maxime expedita, quaerat blanditiis cumque asperiores eaque! Architecto necessitatibus ab delectus, aliquid mollitia fugit possimus esse vel, iusto ut exercitationem. Molestiae libero at corrupti earum aliquam cumque odio laborum, ipsam, nihil recusandae itaque repellat. Omnis asperiores repudiandae, ex culpa provident quisquam vel alias sequi, in at assumenda eum architecto dignissimos repellendus autem error distinctio. Ratione, cumque amet. Officia quam error, illum ipsum amet fuga quidem excepturi velit quae maxime assumenda voluptate, non dolorum exercitationem laudantium, totam harum explicabo deserunt nihil natus nam! Facere tempora perspiciatis incidunt, libero doloremque quo excepturi dolore iste debitis quasi repellendus possimus asperiores nam ut, eum eligendi corrupti temporibus! Necessitatibus debitis natus doloremque mollitia. Quidem rem autem odit repudiandae. Maiores enim minus nisi commodi? Possimus id debitis illo animi, officia dolor. Debitis blanditiis aliquam fugiat! Harum consequatur ipsam earum sapiente cumque quia error doloremque magnam porro aspernatur unde quisquam rem ratione quidem, dolore corporis deserunt cum architecto eius accusamus sunt id. Accusantium suscipit, voluptatibus harum optio, asperiores vitae provident distinctio incidunt nihil similique quis placeat inventore excepturi numquam consectetur necessitatibus natus pariatur maiores a nesciunt, doloribus sed tempora quasi. Vitae quisquam reiciendis maiores aperiam ducimus modi dicta.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TextBlock;
